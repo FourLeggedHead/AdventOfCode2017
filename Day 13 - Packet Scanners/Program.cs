f25d5a9037811a20bcc630bc228285ea02dc609b
Eric Lauret <eric.lauret@live.com>
2021-12-01 23:21:53 +0100

    Solved part I of Day 13

---
+++ Day 13 - Packet Scanners/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day_13___Packet_Scanners
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 13: Packet Scanners");
+
+            var layers = File.ReadAllLines(@".\Resources\input.txt").Select(l => l.Split(':').Select(int.Parse))
+                .ToDictionary(l => l.ElementAt(0), l => l.ElementAt(1));
+
+            // Part I
+            var severity = layers.Select(l => l.Key % ((l.Value - 1) * 2) == 0 ? l.Key * l.Value : 0).Sum();
+            Console.WriteLine(severity);
+        }
+    }
+}
