579586e3ae5068142a782b3065ead49214576569
Eric Lauret <eric.lauret@live.com>
2021-12-01 21:46:34 +0100

    Squashed commit of the following:
    commit bb9de85111d7d33bb84145a6a433a62ffe673d27
    Author: Eric Lauret <eric.lauret@live.com>
    Date:   Wed Dec 1 21:40:38 2021 +0100
    
        Refactored solution for Day 12
    
    commit 7243514a0d16e9759d9d2faa237d480b1aba628a
    Author: Eric Lauret <eric.lauret@live.com>
    Date:   Wed Dec 1 21:31:37 2021 +0100
    
        Changed porject to target .NET Core and be able to contribute with VS Code
    
    commit 7877156555466178d79b18f30631b046f06dc2a0
    Author: Eric Lauret <eric.lauret@live.com>
    Date:   Wed Dec 1 21:27:08 2021 +0100
    
        Added VS Code configuration for running and debugging
    
    commit 3e98ec33d4f33132482bff0b89dfab4a6ff5b0fa
    Author: Eric Lauret <eric.lauret@live.com>
    Date:   Wed Dec 1 21:23:39 2021 +0100
    
        Updated .NET framework to 4.8
    
    commit 077679007af06b1dad6689b4fc8bf3c11e82c240
    Author: Eric Lauret <eric.lauret@live.com>
    Date:   Sun Oct 31 10:36:54 2021 +0100
    
        Added project to solution
    
    commit dff3893fba5a035a63368daefbce438f26625820
    Author: Eric Lauret <eric.lauret@live.com>
    Date:   Sun Oct 31 10:36:43 2021 +0100
    
        Updated input file
    
    commit b18ffb35c6316aaf3666c567606039e57ada54c8
    Author: Eric Lauret <eric.lauret@live.com>
    Date:   Sun Oct 31 10:33:47 2021 +0100
    
        Added existing solution for Day 12

---
+++ Day 12 - DigitalPlumber/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day_12___DigitalPlumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 12: Digital Plumber");
+
+            // Part I
+            var regex = new Regex(@"(?<Id>\d+) <-> (?<ToIds>(\d+,? ?)+)");
+            var programPipesDictionary = File.ReadAllLines(@"Resources\Input.txt").Select(l => regex.Match(l))
+                                    .ToDictionary(k => int.Parse(k.Groups["Id"].Value),
+                                            v => v.Groups["ToIds"].Value.Replace(" ", String.Empty).Split(',').Select(int.Parse).ToList());
+
+            var connectedPrograms = new List<int>();
+            ListConnectedPrograms(0, programPipesDictionary, connectedPrograms);
+
+            Console.WriteLine(connectedPrograms.Count);
+
+            // Part II
+            var programGroupsDictionary = programPipesDictionary.ToDictionary(k => k.Key, v => -1);
+            var programsWithoutGroup = programGroupsDictionary.Where(p => p.Value == -1).Select(p => p.Key).OrderBy(p => p).ToArray();
+            while (programsWithoutGroup.Count() != 0)
+            {
+                AssignGroup(programsWithoutGroup[0], programsWithoutGroup[0], programPipesDictionary, programGroupsDictionary);
+                programsWithoutGroup = programGroupsDictionary.Where(p => p.Value == -1).Select(p => p.Key).OrderBy(p => p).ToArray();
+            }
+
+            Console.WriteLine(programGroupsDictionary.Select(p => p.Value).Distinct().Count());
+        }
+
+        static void ListConnectedPrograms(int rootProgram, Dictionary<int, List<int>> programPipesDictionary, List<int> connectedPrograms)
+        {
+            if (connectedPrograms.Count == 0)
+            {
+                connectedPrograms.Add(rootProgram);
+            }
+
+            foreach (var pipe in programPipesDictionary[rootProgram])
+            {
+                if (connectedPrograms.Contains(pipe)) continue;
+                connectedPrograms.Add(pipe);
+                ListConnectedPrograms(pipe, programPipesDictionary, connectedPrograms);
+            }
+        }
+
+        static void AssignGroup(int rootProgram, int groupId, Dictionary<int, List<int>> programPipesDictionary, Dictionary<int, int> programGroupsDictionary)
+        {
+            if (programGroupsDictionary[rootProgram] == -1)
+            {
+                programGroupsDictionary[rootProgram] = groupId;
+
+                foreach (var pipe in programPipesDictionary[rootProgram])
+                {
+                    AssignGroup(pipe, groupId, programPipesDictionary, programGroupsDictionary);
+                }
+            }
+        }
+    }
+}
